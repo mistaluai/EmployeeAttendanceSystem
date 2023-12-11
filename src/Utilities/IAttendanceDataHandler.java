f34b9df45a3953ff7f69de9c6fbaa95d84b68bc4
Luai Waleed <mistaluai@gmail.com>
2023-12-11 20:41:09 +0200

    added interfaces for important utilities

---
+++ src/Utilities/IAttendanceDataHandler.java
@@ -0,0 +1,20 @@
+package Utilities;
+
+import java.util.List;
+
+public interface IAttendanceDataHandler {
+    /**
+     * Reads the employee attendance record data
+     * @param ID: Target employee ID
+     * @return: returns list of attendance records
+     */
+    List<AttendanceRecord> getRecords(int ID);
+
+    /**
+     * updates an existing attendance record of an employee
+     * @param ID: Target employee ID
+     * @param updatedRecord: the updated record to replace the old record
+     */
+    void updateRecord(int ID, AttendanceRecord updatedRecord);
+
+}
---
+++ src/Utilities/ILogger.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public interface ILogger {
+    /**
+     * writes a log to the logs file
+     * @param log: String message to be written to the log
+     */
+    void writeLog(String log);
+
+    /**
+     * writes error to the error file
+     * @param stackTrace: String containing the stack trace of the error
+     */
+    void writeError(String stackTrace);
+}
