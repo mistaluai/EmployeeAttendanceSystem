c483ba6efcc05ba48858b34b0b8d337dff2e892c
‪Ziad‬‏ Nasser <ziadnasser019@gmail.com>
2023-12-13 23:24:49 +0200

    I implement View and SuperView methods in Manager

--- src/Backend/AttendanceManagment/AttendanceManager.java
+++ src/Backend/AttendanceManagment/AttendanceManager.java
@@ -4,6 +4,7 @@ import Backend.AttendanceManagment.AttendanceStates.IAttendanceState;
 import Utilities.DataHandling.AttendanceFileHandler;
 import Utilities.DataHandling.IAttendanceDataHandler;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class AttendanceManager implements IAttendanceManager {
@@ -22,10 +23,21 @@ public class AttendanceManager implements IAttendanceManager {
     }
 
     public String [][] viewAttendanceHistory() {
-
+        return superViewAttendanceHistory(employeeID);
     }
 
     public String [][] superViewAttendanceHistory(int ID) {
-
+        IAttendanceDataHandler attendanceDataHandler = new AttendanceFileHandler();
+        attendanceRecords = attendanceDataHandler.getRecords(ID);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+        String [][] records = new String[attendanceRecords.size()][3];
+        for (int i = 0; i < attendanceRecords.size(); i++) {
+            AttendanceRecord myRecord = attendanceRecords.get(i);
+            records[i][0] = dateFormat.format(myRecord.getDate());
+            records[i][1] = timeFormat.format(myRecord.getTimeIn());
+            records[i][2] = timeFormat.format(myRecord.getTimeOut());
+        }
+        return records;
     }
 }
