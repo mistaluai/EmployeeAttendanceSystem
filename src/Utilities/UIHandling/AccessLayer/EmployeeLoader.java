185dcf6678d63ab9b7f39638935e5bb0a3c864b6
Amr <111288164+BlindFl0W@users.noreply.github.com>
2023-12-14 01:00:54 +0200

    The EmployeeLoader Edit. - Now The EmployeeLoader returns null if the ID wasn't found.

--- src/Utilities/UIHandling/AccessLayer/EmployeeLoader.java
+++ src/Utilities/UIHandling/AccessLayer/EmployeeLoader.java
@@ -9,6 +9,7 @@ public class EmployeeLoader implements IEmployeeLoader {
      * This method is used to decide if the object
      * should be created as an employee or a supervisor
      * based on the employee's data.
+     * returns null if the ID wasn't found.
      * @param ID: target employee's ID
      * @return: The object
      */
@@ -17,8 +18,8 @@ public class EmployeeLoader implements IEmployeeLoader {
         String employeeType = new AttendanceFileHandler().readEmployeeData(ID)[5];
         if (Integer.parseInt(employeeType) == 1){
             return new Supervisor(ID);
-        } else {
+        } else if (Integer.parseInt(employeeType) == 0) {
             return new Employee(ID);
-        }
+        }return null;
     }
 }
