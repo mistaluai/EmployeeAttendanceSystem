87efff0c4cd2e21cdb6bbe9e52e9b2939b212a45
ZiadMohamed9 <zm76759@gmail.com>
2023-12-15 20:06:01 +0200

    made Department record, improved the usage of command in Employee and Supervisor by adding an attribute IAttendanceCommand and a setter to change the command in every method.

--- src/Backend/Entities/Department.java
+++ src/Backend/Entities/Department.java
@@ -2,38 +2,8 @@ package Backend.Entities;
 
 /**
  * The Department class represents a department within the company.
+ *
+ * @param departmentName Private fields The name of the department
+ * @param supervisor     The supervisor of the department
  */
-public class Department {
-    // Private fields
-    private final String departmentName; // The name of the department
-    private final String supervisor; // The supervisor of the department
-
-    /**
-     * Constructs a Department object with a department name and supervisor.
-     *
-     * @param departmentName The name of the department.
-     * @param supervisor     The supervisor of the department.
-     */
-    public Department(String departmentName, String supervisor) {
-        this.departmentName = departmentName;
-        this.supervisor = supervisor;
-    }
-
-    /**
-     * Retrieves the name of the department.
-     *
-     * @return The name of the department.
-     */
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    /**
-     * Retrieves the supervisor of the department.
-     *
-     * @return The supervisor of the department.
-     */
-    public String getSupervisor() {
-        return supervisor;
-    }
-}
+public record Department(String departmentName, String supervisor) {}
