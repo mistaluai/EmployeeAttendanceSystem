97d8f12022fd6933235ad4ecb9184f8468847eff
Amr <111288164+BlindFl0W@users.noreply.github.com>
2023-12-15 11:04:11 +0200

    Fixing The Data Transfer Issue
    - Add DTO data transfer object class that contains a list of attendance records
    and can return a 2D array to be used in the front end.
    - Edited all the related classes, interfaces and methods from useing
    a referance of a window object to using the DTO.
    
    This edit will fixed the issue.

---
+++ src/DTO/DTO.java
@@ -0,0 +1,37 @@
+package DTO;
+
+import Backend.AttendanceManagment.AttendanceRecord;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class DTO {
+    private List<AttendanceRecord> attendanceRecords;
+
+    public DTO(List<AttendanceRecord> attendanceRecords) {
+        this.attendanceRecords = attendanceRecords;
+    }
+
+    public List<AttendanceRecord> getAttendanceRecords() {
+        return attendanceRecords;
+    }
+
+    public void setAttendanceRecords(List<AttendanceRecord> attendanceRecords) {
+        this.attendanceRecords = attendanceRecords;
+    }
+
+    public String[][] getAttendanceRecordsArray() {
+        String[][] attendanceHistory = new String[attendanceRecords.size()][3];
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+
+        for (int i = 0; i < attendanceRecords.size(); i++) {
+            AttendanceRecord myRecord = attendanceRecords.get(i);
+            attendanceHistory[i][0] = dateFormat.format(myRecord.getDate());
+            attendanceHistory[i][1] = timeFormat.format(myRecord.getTimeIn());
+            attendanceHistory[i][2] = timeFormat.format(myRecord.getTimeOut());
+        }
+
+        return attendanceHistory;
+    }
+}
